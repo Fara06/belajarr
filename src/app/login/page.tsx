5747ef97c1ce33e6621c4c3832e6022fd9bcbd24
fara06 <fara.ramafa@gmail.com>
2025-05-20 15:18:56 +0700

    second

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
 import Image from 'next/image';
 import MicIcon from '@mui/icons-material/Mic';
 import AssessmentIcon from '@mui/icons-material/Assessment';
@@ -8,12 +7,19 @@ import InsightsIcon from '@mui/icons-material/Insights';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { InputAdornment, Typography, Button, Box, Modal, Alert, AlertTitle, Link, FormControl, InputLabel, OutlinedInput, TextField, IconButton } from '@mui/material';
+import { Stack, InputAdornment, Typography, Button, Box, Modal, Alert, AlertTitle, Link, FormControl, InputLabel, OutlinedInput, TextField, IconButton, } from '@mui/material';
+// import {SignInPage, type AuthProvider} from '@toolpad/core/SignInPage';
+// import {AppProvider} from '@toolpad/core/AppProvider';
+// import { useTheme } from '@emotion/react';
 
 export default function Login() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const validateEmail = (email) => {
+    const emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -26,19 +32,18 @@ export default function Login() {
   const [password, setPassword ] = React.useState(false)
   const handleSignIn = () => {
     if (email && password) {
-      console.log('Sign In');
+      console.log('Email:', email);
     } else {
-      console.log('Please enter your email and password');
+      console.log('Password:', password);
     }
     setOpen(true);
   };
   return (
-    <main className="flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen gap-14">\
-    
-      <div className="w-full max-w-md">
+    <main className="flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen gap-14 bg-white">
+      <div className="w-full max-w-md text-gray-500">
         <Stack spacing={2}>
           <Stack direction="row" spacing={2}>
-            <MicIcon fontSize="medium" />
+            <MicIcon fontSize="medium"/>
             <Box>
               <Typography variant="subtitle2">Voice Detection</Typography>
               <Typography variant="body2">
@@ -75,10 +80,11 @@ export default function Login() {
           </Stack>
         </Stack>
       </div>
+      
       <div className="w-full max-w-md flex flex-col items-center">
-        <Image src="/next.svg" alt="Logo" width={170} height={170} className='mb-4' />
+        <Image src="/next.svg" alt="Logo" width={170} height={170} className='right-0 opacity-75 mt6 mb-1' />
         <Typography variant="h4" className="text-2xl font-bold text-gray-600 mt-2 mb-1 text-center">
-          Welcome to Gank! 👋🏻
+          Welcome to NextJS! 👋🏻
         </Typography>
         <Typography variant="subtitle1" className="text-gray-500 mb-4 text-center">
           Please sign in to your account and start your adventure!
@@ -88,6 +94,7 @@ export default function Login() {
           label="Email"
           type="email"
           margin="normal"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <FormControl variant="outlined" fullWidth sx={{ mt: 1 }}>
@@ -115,7 +122,7 @@ export default function Login() {
         <Link href="#" color="secondary" className="self-end mt-2 mb-4 text-sm">
           Forgot Password?
         </Link>
-        <Button onClick={handleOpen} color="secondary" variant="contained" fullWidth {handleSignIn}>
+        <Button onClick={handleSignIn} color="secondary" variant="contained" fullWidth>
           Sign in
         </Button>
         <Modal
